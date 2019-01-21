1d6bdfa926823423e849081d56098ddb3c20736d
Bro4all <omar.habra@sjsu.edu>
2019-01-20 20:53:52 -0800

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Omar Habra on 1/20/19.
+//  Copyright © 2019 Omar Habra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
